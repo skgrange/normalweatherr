892688821851da989c318d817811872f887191bf
skgrange <skgrange@gmail.com>
2017-11-08 20:21:51 +0100

    add theil_sen_trend_vector

--- R/theil_sen_trend_test.R
+++ R/theil_sen_trend_test.R
@@ -4,8 +4,8 @@
 #' 
 #' @param variable Variable name to test. 
 #' 
-#' @param deseason Should the time series be deseaonsalised before the trend test
-#' is conducted. 
+#' @param deseason Should the time series be deseaonsalised before the trend 
+#' test is conducted?
 #' 
 #' @param auto_correlation Should auto correlation be considered in the estimates?
 #' 
@@ -40,7 +40,7 @@ theil_sen_trend_test <- function(df, variable = "value", deseason = FALSE,
   names(df_test) <- stringr::str_replace_all(names(df_test), "\\.", "_")
   
   # Remove duplicate observations
-  df_test <- dplyr::filter(df_test, is.finite(conc))
+  df_test <- filter(df_test, is.finite(conc))
   
   # Add a variable
   df_test$direction <- ifelse(df_test$slope <= 0, "decreasing", "increasing")
@@ -76,4 +76,55 @@ quiet <- function (x) {
   sink(tempfile())
   on.exit(sink())
   invisible(force(x))
+}
+
+
+#' Function to calculate a trend vector for a time series. 
+#' 
+#' @param df Input data frame. 
+#' 
+#' @param variable Variable to test. 
+#' 
+#' @param deseason Should the time series be deseaonsalised before the trend 
+#' test is conducted?
+#' 
+#' @param auto_correlation Should auto correlation be considered in the estimates?
+#' 
+#' @param df_trend An existing trend data frame from 
+#' \code{\link{theil_sen_trend_test}}. An optional argument. 
+#' 
+#' @seealso \code{\link{theil_sen_trend_test}}
+#' 
+#' @return Numeric vector with the length of \code{nrow(df)}. 
+#' 
+#' @author Stuart K. Grange
+#'
+#' @export
+theil_sen_trend_vector <- function(df, variable = "value", deseason = FALSE, 
+                                   auto_correlation = FALSE, df_trend = NA) {
+  
+  if (!"date" %in% names(df))
+    stop("`df` must have a `date` variable...", call. = FALSE)
+  
+  # Get date vector
+  date <- as.numeric(df$date)
+  
+  # Do trend test if needed
+  if (is.na(df_trend[1])) {
+    
+    df_trend <- theil_sen_trend_test(
+      df, 
+      variable = variable,
+      deseason = deseason,
+      auto_correlation = auto_correlation
+    )
+    
+  }
+  
+  # Equation of a staight line
+  # Coefficent is number of seconds in a year
+  value_predict <- df_trend$slope / 31536000 * date + df_trend$intercept
+  
+  return(value_predict)
+  
 }
